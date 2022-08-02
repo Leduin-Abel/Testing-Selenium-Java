2af85e345fdeddf6e5e33016e947fa84829072a0
Leduin-Abel <l.abel.bejaranop@gmail.com>
2022-08-01 20:24:57 -0500

    Web elements

--- Project/src/test/java/base/BaseTests.java
+++ Project/src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -11,9 +15,15 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver","chromedriver_folder/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");//lanza el webdriver y carga un url
-        driver.manage().window().maximize(); //maximiza la ventana, manage.window permite modificar el tamaño de la ventana
+       // driver.manage().window().maximize(); //maximiza la ventana, manage.window permite modificar el tamaño de la ventana
+        //WebElement inputsLink = driver.findElement(By.linkText("Inputs")); //Retorna  solo un elemento de web el primero que encuentre, "By.... es mediante qué lo busca"
+        //inputsLink.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
         System.out.println(driver.getTitle()); //getTitle Obtiene el titulo de la aplicacion
-        driver.quit(); //cierra el navegador y termina la sesion
+        driver.quit(); //cierra el navegador y termina la sesionk
     }
 
     public static void main(String args[]) {
